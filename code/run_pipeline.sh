edc29ea3034e744912e0e1090e5f0077360f3e59
Ankur Aggarwal <agankur21@gmail.com>
2016-12-05 20:18:14 -0500

    Changed code to accomodate new change sin Caffenet instead of Alexnet

--- code/run_pipeline.sh
+++ code/run_pipeline.sh
@@ -12,7 +12,7 @@ $CAFFE_TOOLS_DIR/convert_imageset -resize_height=227 -resize_width=227 -shuffle=
 $CAFFE_TOOLS_DIR/convert_imageset -resize_height=227 -resize_width=227 -shuffle=true ${TRAIN_DATA_DIR}/ ${ROOT_DATA_DIR}/train_data.txt ${ROOT_DATA_DIR}/train_lmdb
 
 #Compute Image mean :
-${CAFFE_TOOLS_DIR}/compute_image_mean -backend=lmdb ${ROOT_DATA_DIR}/train_lmdb ${ROOT_DATA_DIR}/train_mean.binaryproto
+${CAFFE_TOOLS_DIR}/compute_image_mean -backend=lmdb ${ROOT_DATA_DIR}/train_lmdb ${ROOT_DATA_DIR}/mean_all.binaryproto
 
 #Train model
 ${CAFFE_TOOLS_DIR}/caffe train --solver ${CAFFE_MODELS_DIR}/bvlc_reference_caffenet/solver_spatial.prototxt --weights $CAFFE_MODELS_DIR/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel 2>&1 | tee ${ROOT_DATA_DIR}/spatial_cnn.log
